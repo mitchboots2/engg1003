6265bb0a522f5f2fa51bec1a7f3ecb73857be821
Mitch Boots <c3350609@uon.edu.au>
2020-02-24 19:26:58 +1100

    hmjum

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// hey
+int main()
+{
+    float y;
+    float x;
+
+    printf("Enter an number:");
+    scanf("%f", &x);
+    y = x/0;
+    printf("y:%f\n", y);
+    return 0;//hey
+}
